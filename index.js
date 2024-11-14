f4e4d468ca0831b56761169144fbb2bde235d94a
fadouma ahmed <fannyahmed85@gmail.com>
2024-11-13 19:53:52 -0700

    complete

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Code your solution here
+function findMatching(arrOfDrivers, string) {
+     return arrOfDrivers.filter(driver => driver.toLowerCase() === string.toLowerCase());
+     }
+function fuzzyMatch(arrOfDrivers, letters) {
+     return arrOfDrivers.filter(driver => driver.startsWith(letters)); 
+    }
+function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+}
